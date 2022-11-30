3538355d068dee929cd2be1cf3faa7d5017b31fe
Sang Yoon <sangyun0904@gmail.com>
2022-11-30 21:45:33 +0900

    add greeting

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const LoginForm = document.querySelector("#Login-form");
+const LoginInput = document.querySelector("#Login-form input");
 const LoginButton = document.querySelector("#Login-form button");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(tomato) {
     tomato.preventDefault();
-    console.log(tomato);
-}
+    const username = LoginInput.value;
+    LoginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username} !`; // not '' or "" but only `` !!!!!
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    console.log(username);
+} 
 
 LoginForm.addEventListener("submit", onLoginSubmit);
