4e2b9206ef4bd49213447966ecd557bb53345a45
Sang Yoon <sangyun0904@gmail.com>
2022-11-30 13:41:02 +0900

    Add simple login page

---
+++ app.js
@@ -0,0 +1,9 @@
+const LoginInput = document.querySelector("#Login-form input");
+const LoginButton = document.querySelector("#Login-form button");
+
+
+function onLoginBtnClick() {
+    console.log("click!!!!")
+}
+
+LoginButton.addEventListener("click", onLoginBtnClick)
