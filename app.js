47764f1c5a914a86643a51ef8c346b39c6c23a14
sangyoon kim <sangyun0904@gmail.com>
2022-12-03 18:21:17 +0900

    Add check user login

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ const LoginButton = document.querySelector("#Login-form button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(tomato) {
     tomato.preventDefault();
     const username = LoginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     LoginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello ${username} !`; // not '' or "" but only `` !!!!! mac : ₩
     greeting.classList.remove(HIDDEN_CLASSNAME);
     console.log(username);
 } 
 
-LoginForm.addEventListener("submit", onLoginSubmit);
+const savedUSername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUSername === null) {
+    LoginForm.classList.remove(HIDDEN_CLASSNAME);
+    LoginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${savedUSername} !`;
+}
