e1a7e18065ea5f264c2bb0df27777ae060378a20
sangyoon kim <sangyun0904@gmail.com>
2022-12-10 21:15:59 +0900

    add saveTodos using JSON stringify

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveTodos() {
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -16,7 +22,6 @@ function paintToDo(newTodo) {
     li.appendChild(span);
     li.appendChild(button);
     span.innerText = newTodo;
-    console.log(li);
     toDoList.appendChild(li);
 }
 
@@ -24,7 +29,9 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveTodos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
